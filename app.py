329ad382c615f434a54869fbb2e19a08747d150a
Tarun <93933830+tarunkurapati1404@users.noreply.github.com>
2026-04-21 17:32:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, request
+import os
+
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, Secure World!"
+@app.route('/ping')
+def ping():
+    # Grabbing user input from the website URL
+    host = request.args.get('host')
+    
+    # VULNERABILITY: Command Injection! 
+    # Never put user input directly into an OS command
+    os.system("ping -c 1 " + host)
+    
+    return "Pinged the host!"
 
 if __name__ == '__main__':
     app.run(debug=True)
