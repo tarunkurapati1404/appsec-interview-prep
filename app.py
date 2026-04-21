ab9466123e5a5927627a92ecca908365276b2a58
Tarun <93933830+tarunkurapati1404@users.noreply.github.com>
2026-04-21 17:39:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
-from flask import Flask, request
-import os
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/ping')
-def ping():
-    # Grabbing user input from the website URL
-    host = request.args.get('host')
-    
-    # VULNERABILITY: Command Injection! 
-    # Never put user input directly into an OS command
-    os.system("ping -c 1 " + host)
-    
-    return "Pinged the host!"
+# VULNERABILITY: Hardcoded Password!
+db_password = "SuperSecretAdminPassword123!"
+db_user = "admin"
+
+@app.route('/')
+def home():
+    return "Connected to the database!"
 
 if __name__ == '__main__':
     app.run(debug=True)
