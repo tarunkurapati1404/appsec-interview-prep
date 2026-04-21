52f254b891c575fcd8d3ddaadb9424426566094e
Tarun <93933830+tarunkurapati1404@users.noreply.github.com>
2026-04-21 18:06:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
-# VULNERABILITY: Hardcoded Password!
-db_password = "SuperSecretAdminPassword123!"
-db_user = "admin"
+# SECURE: Pulling the password from an Environment Variable
+db_password = os.environ.get("DB_PASSWORD", "default_placeholder")
+db_user = os.environ.get("DB_USER", "default_placeholder")
 
-@app.route('/')
+# SECURE: Specifying that this route ONLY accepts GET requests
+@app.route('/', methods=['GET'])
 def home():
-    return "Connected to the database!"
+    return "Connected to the database securely!"
 
 if __name__ == '__main__':
     app.run(debug=True)
