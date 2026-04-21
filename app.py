b5937e8cb5af3a60860cb4393fa33dd803460bb9
Tarun <93933830+tarunkurapati1404@users.noreply.github.com>
2026-04-21 18:25:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 # SECURE: Pulling the password from an Environment Variable
 db_password = os.environ.get("DB_PASSWORD", "default_placeholder")
-db_user = os.environ.get("DB_USER", "default_placeholder")
+db_user = os.environ.get("DB_USER", "default_placeholder") #fixed
 
 # SECURE: Specifying that this route ONLY accepts GET requests
 @app.route('/', methods=['GET'])
